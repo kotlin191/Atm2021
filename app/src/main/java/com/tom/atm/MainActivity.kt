d56e1abe6ee6ef4d18d9ca575bdbfd9ba899f90e
Hank Tom <hanktom@gmail.com>
2020-12-08 23:45:26 +0800

    login 登入

--- app/src/main/java/com/tom/atm/MainActivity.kt
+++ app/src/main/java/com/tom/atm/MainActivity.kt
@@ -1,11 +1,18 @@
 package com.tom.atm
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    var login = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (!login) {
+            Intent(this, LoginActivity::class.java).apply {
+                startActivity(this)
+            }
+        }
     }
 }
