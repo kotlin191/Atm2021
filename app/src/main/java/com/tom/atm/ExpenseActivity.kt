30d0eb5a6be07f1fea431e6feaa0db1e02dc4003
Hank Tom <hanktom@gmail.com>
2020-12-09 19:26:25 +0800

    協程查詢資料庫並以RecyclerView展示

--- app/src/main/java/com/tom/atm/ExpenseActivity.kt
+++ app/src/main/java/com/tom/atm/ExpenseActivity.kt
@@ -3,8 +3,14 @@ package com.tom.atm
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import androidx.room.Room
 import kotlinx.android.synthetic.main.activity_expense.*
+import kotlinx.android.synthetic.main.expense_row.view.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -31,11 +37,42 @@ class ExpenseActivity : AppCompatActivity() {
         CoroutineScope(Dispatchers.IO).launch {
             val expenses = database.expenseDao().getAll()
             Log.d(TAG, expenses.size.toString())
+            val adapter = object : RecyclerView.Adapter<ExpenseViewHolder>() {
+                override fun onCreateViewHolder(parent: ViewGroup, viewType: Int)
+                            : ExpenseViewHolder {
+                    val view = LayoutInflater.from(parent.context)
+                        .inflate(R.layout.expense_row, parent, false)
+                    return ExpenseViewHolder(view)
+                }
+
+                override fun onBindViewHolder(holder: ExpenseViewHolder, position: Int) {
+                    val exp = expenses.get(position)
+                    holder.date.setText(exp.date)
+                    holder.info.setText(exp.info)
+                    holder.amount.setText(exp.amount.toString())
+                }
+
+                override fun getItemCount(): Int {
+                    return expenses.size
+                }
+            }
+            runOnUiThread {
+                recycler.setHasFixedSize(true)
+                recycler.layoutManager = LinearLayoutManager(this@ExpenseActivity)
+                recycler.adapter = adapter
+            }
         }
         fab.setOnClickListener {
             Executors.newSingleThreadExecutor().execute {
                 database.expenseDao().add(expenseData[0])
             }
         }
+    }
+
+    class ExpenseViewHolder(itemView: View)
+        : RecyclerView.ViewHolder(itemView) {
+        val date = itemView.exp_date
+        val info = itemView.exp_info
+        val amount = itemView.exp_amount
     }
 }
