d26dc15a7176a8b8b9bfe497f8889e632c638ddb
Hank Tom <hanktom@gmail.com>
2020-12-09 18:48:50 +0800

    導入Coroutines 協程, 測試資料庫查詢

--- app/src/main/java/com/tom/atm/ExpenseActivity.kt
+++ app/src/main/java/com/tom/atm/ExpenseActivity.kt
@@ -2,11 +2,19 @@ package com.tom.atm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.room.Room
 import kotlinx.android.synthetic.main.activity_expense.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.util.concurrent.Executors
 
 class ExpenseActivity : AppCompatActivity() {
+    companion object {
+        val TAG = ExpenseActivity::class.java.simpleName
+    }
+    private lateinit var database: ExpenseDatabase
     val expenseData = arrayListOf<Expense>(
         Expense("2021/02/01", "Lunch", 120),
         Expense("2021/02/02", "停車費", 60),
@@ -17,10 +25,14 @@ class ExpenseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_expense)
+        database = Room.databaseBuilder(this,
+            ExpenseDatabase::class.java, "expense.db")
+            .build()
+        CoroutineScope(Dispatchers.IO).launch {
+            val expenses = database.expenseDao().getAll()
+            Log.d(TAG, expenses.size.toString())
+        }
         fab.setOnClickListener {
-            val database = Room.databaseBuilder(this,
-                ExpenseDatabase::class.java, "expense.db")
-                .build()
             Executors.newSingleThreadExecutor().execute {
                 database.expenseDao().add(expenseData[0])
             }
