38f6a01a90694ab119cc4583c7da193c809c603f
Hank Tom <hanktom@gmail.com>
2020-12-09 20:34:31 +0800

    網路登入與協程

--- app/src/main/java/com/tom/atm/LoginActivity.kt
+++ app/src/main/java/com/tom/atm/LoginActivity.kt
@@ -2,12 +2,21 @@ package com.tom.atm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.net.URL
 
 class LoginActivity : AppCompatActivity() {
+    companion object {
+        val TAG = LoginActivity::class.java.simpleName
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
@@ -17,26 +26,36 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun login(view: View) {
+        //https://atm201605.appspot.com/login?uid=帳號&pw=密碼
         val userid = ed_userid.text.toString()
         val passwd = ed_passwd.text.toString()
-        if (userid == "jack" && passwd == "1234") {
-            Toast.makeText(this, "登入成功", Toast.LENGTH_LONG).show()
-            getSharedPreferences("atm", MODE_PRIVATE)
-                .edit()
-                .putString("PREF_USERID", userid)
-                .apply()
-            intent.putExtra("LOGIN_USERID", userid)
-            intent.putExtra("LOGIN_PASSWD", passwd)
-            setResult(RESULT_OK, intent)
-            finish()
-        } else {
-            AlertDialog.Builder(this)
-                .setTitle("ATM")
-                .setMessage("登入失敗")
-                .setPositiveButton("OK", null)
-                .show()
+        CoroutineScope(Dispatchers.IO).launch {
+            val result =
+                URL("https://atm201605.appspot.com/login?uid=$userid&pw=$passwd")
+                    .readText()
+            Log.d(TAG, result)
+            if (result == "1") {
+                getSharedPreferences("atm", MODE_PRIVATE)
+                    .edit()
+                    .putString("PREF_USERID", userid)
+                    .apply()
+                runOnUiThread {
+                    Toast.makeText(this@LoginActivity, "登入成功", Toast.LENGTH_LONG).show()
+                }
+                intent.putExtra("LOGIN_USERID", userid)
+                intent.putExtra("LOGIN_PASSWD", passwd)
+                setResult(RESULT_OK, intent)
+                finish()
+            } else {
+                runOnUiThread {
+                    AlertDialog.Builder(this@LoginActivity)
+                        .setTitle("ATM")
+                        .setMessage("登入失敗")
+                        .setPositiveButton("OK", null)
+                        .show()
+                }
+            }
         }
-
     }
 
     fun cancel(view: View) {
