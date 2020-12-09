6ed1ded623090af8b875630d0a4348363de9832d
Hank Tom <hanktom@gmail.com>
2020-12-09 20:52:04 +0800

    OkHttp for JSON

--- app/src/main/java/com/tom/atm/TransActivity.kt
+++ app/src/main/java/com/tom/atm/TransActivity.kt
@@ -6,6 +6,8 @@ import android.util.Log
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import okhttp3.OkHttpClient
+import okhttp3.Request
 import java.io.BufferedReader
 import java.net.URL
 
@@ -16,13 +18,22 @@ class TransActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_trans)
+        val client = OkHttpClient.Builder()
+            .build()
+        val request = Request.Builder()
+            .url("https://atm201605.appspot.com/h")
+            .build()
         CoroutineScope(Dispatchers.IO).launch {
+            var response = client.newCall(request).execute()
+            response.body?.run {
+                Log.d(TAG, string())
+            }
+            /*
             val reader = URL("https://atm201605.appspot.com/h")
                 .openConnection()
                 .getInputStream().bufferedReader()
             val json = reader.use(BufferedReader::readText)
-            Log.d(TAG, json)
+            Log.d(TAG, json)*/
         }
-
     }
 }
